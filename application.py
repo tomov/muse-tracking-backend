d6164b43cebfc719887046629d8bd404ab351382
Momchil Tomov <tomov90@gmail.com>
2018-12-24 18:29:24 -0500

    extract_signal_for_comparison working!

--- application.py
+++ application.py
@@ -63,6 +63,13 @@ def insert(query):
     conn.commit()
     return str(rv)
 
+def select(query):
+    conn = mysql.connection
+    cur = conn.cursor()
+    cur.execute(query) 
+    rv = cur.fetchall()
+    return rv
+
 def insert_eeg(table, subject_id, timestamp, utimestamp, eeg1, eeg2, eeg3, eeg4, aux1, aux2):
     query = '''INSERT INTO %s (subject_id, timestamp, utimestamp, eeg1, eeg2, eeg3, eeg4, aux1, aux2) VALUES (%d, from_unixtime(%d), %d, %f, %f, %f, %f, %f, %f)''' % (table, subject_id, timestamp, utimestamp, eeg1, eeg2, eeg3, eeg4, aux1, aux2)
     rv = insert(query)
@@ -154,6 +161,134 @@ def log():
    
     # return 'hiiiiii ' + str(data) + '\n\n\n\n\n' + str(rv)
     return 'OK'
+
+
+# extract rolling average from signal s1 with timestamps t1
+# using rolling window size w1
+# and correspondingly for signal s2 but after a time lag of lag
+#
+# s3 is the hsi scores for s1, with timestamps t3, "flanking" window of s1 (w1) by w3 on each side
+#
+# s = signal
+# t = timestamps
+# w = window size
+#
+def extract_for_comparison(s1, t1, w1, lag, s2, t2, w2, s3, t3, w3):
+    l1 = 0 # first index within current rolling window
+    r1 = 1 # 1 + last index within current rolling window
+    l2 = 0
+    r2 = 1
+    l3 = 0
+    r3 = 1
+    assert(len(s1) == len(t1))
+    assert(len(s2) == len(t2))
+    assert(len(s3) == len(t3))
+
+    s1_win = s1[0] # sum of s1 within rolling window
+    s2_win = s2[0]
+    s3_win = s3[0]
+
+    ret1 = []
+    ret2 = []
+    ret3 = []
+
+    while True:
+
+        # expand rolling window of signal 1 until we span an interval w1 (but not larger)
+        #
+        while r1 < len(s1) and t1[r1] - t1[l1] <= w1:
+            s1_win += s1[r1]
+            r1 += 1
+
+        # shift rolling window of signal 2 until it's at least lag away from window of signal 1
+        #
+        while l2 + 1 < len(s2) and t2[l2] - t1[r1 - 1] < lag:
+            s2_win -= s2[l2]
+            l2 += 1
+
+        # if we can't shift rolling window 2 any more -> break
+        #
+        if l2 + 1 >= len(s2) and t2[l2] - t1[r1 - 1] < lag:
+            break
+
+        # expand rolling window of signal 2 until we span an interval w2 (but not larger)
+        #
+        while r2 < len(s2) and t2[r2] - t2[l2] <= w2:
+            s2_win += s2[r2]
+            r2 += 1
+
+        # shift rolling window of s3 to w3 before window of s1 
+        #
+        while l3 + 1 < len(s3) and t3[l3] < t1[l1] - w3:
+            s3_win -= s3[l3]
+            l3 += 1
+
+        # expand rolling window of s3 to w3 after window of s1
+        #
+        while r3 < len(s3) and t3[r3] <= t1[r1 - 1] + w3:
+            s3_win += s3[r3]
+            r3 += 1
+
+        # if conditions are satisfied, add data points
+        #
+        if t1[r1 - 1] - t1[l1] <= w1 and t2[r2 - 1] - t2[l2] <= w2 and t2[l2] - t1[r1 - 1] >= lag and l3 < r3:
+            s1_avg = s1_win / (r1 - l1)
+            s2_avg = s2_win / (r2 - l2)
+            s3_avg = s3_win / (r3 - l3)
+            if s3_avg <= 2: # avg HSI should be better than 2
+                ret1.append(s1_avg)
+                ret2.append(s2_avg)
+                ret3.append(s3_avg)
+        #        print '                add', (r1 - l1), '  ', (r2 - l2), ' ', (r3 - l3)
+
+        #print l1, r1, l2, r2, l3, r3, '  -> ', t1[r1 - 1] - t1[l1], t2[l2] - t1[r1 - 1], t2[r2 - 1] - t2[l2], t3[r3 - 1] - t3[l3], '   -- ', t3[l3], '[', t1[l1], t1[r1], ']', t3[r3]
+
+        # shift over rolling window of signal 1 
+        #
+        s1_win -= s1[l1]
+        l1 += 1
+
+
+    return ret1, ret2, ret3
+
+
+@application.route('/query_test')
+def query_test():
+    rv = select('''SELECT eeg1, utimestamp / 1000 FROM theta LIMIT 10000''')
+    s1 = []
+    t1 = []
+    for row in rv:
+        s1.append(row[0])
+        t1.append(row[1])
+    #w1 = 250
+    w1 = 500
+
+    rv = select('''SELECT SQRT(POW(x, 2) + POW(y, 2) + POW(z, 2)), utimestamp / 1000  FROM accelerometer LIMIT 10000''')
+    s2 = []
+    t2 = []
+    for row in rv:
+        s2.append(row[0])
+        t2.append(row[1])
+    #w2 = 250
+    w2 = 500
+
+    rv = select('''SELECT eeg1, utimestamp / 1000 FROM hsi LIMIT 10000''')
+    s3 = []
+    t3 = []
+    for row in rv:
+        s3.append(row[0])
+        t3.append(row[1])
+    w3 = 500
+
+    lag = 0
+    ret1, ret2, ret3 = extract_for_comparison(s1, t1, w1, lag, s2, t2, w2, s3, t3, w3)
+    return str(len(ret1))
+
+
+@application.route('/viz')
+def viz():
+    return ''
+
 
 # TODO use TCP sockets --> https://realpython.com/python-sockets/#echo-server
 
